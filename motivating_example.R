5e675f5ea24af71d0e38cd0e15baf2437c23f46d
bcolnet <benedicte.colnet@gmail.com>
2021-11-10 09:14:46 +0100

    error in the subset

--- motivating_example.R
+++ motivating_example.R
@@ -33,28 +33,49 @@ for (sample.size in c(300, 1000, 3000, 9000, 30000)){
       
       # generate a simulation
       if (simulation == "linear"){
+        
         a_simulation <- generate_simulation_linear(n_obs = sample.size, independent_covariate = FALSE)
+        
+        # choose subset
+        for (method in different_subset_tested){
+          if (method == "outcome.and.instruments"){
+            X_treatment <- paste0("X.", 1:10)
+            X_outcome <- paste0("X.", 1:10)
+          } else if (method == "outcome.wo.instruments"){
+            X_treatment <- paste0("X.", 4:10)
+            X_outcome <- paste0("X.", 4:10)
+          } else if (method == "smart"){
+            X_treatment <- paste0("X.", 4:7)
+            X_outcome <- paste0("X.", 4:10)
+          } else if (method == "minimal.set"){
+            
+          } else {
+            stop("error in subset.")
+          }
+          
       } else {
+        
         a_simulation <-  generate_simulation_wager_nie(n = sample.size, setup = "C")
+        
+        # choose subset
+        for (method in different_subset_tested){
+          if (method == "outcome.and.instruments"){
+            X_treatment <- paste0("X.", 1:6)
+            X_outcome <- paste0("X.", 1:6)
+          } else if (method == "outcome.wo.instruments"){
+            X_treatment <- paste0("X.", 2:6)
+            X_outcome <- paste0("X.", 2:6)
+          } else if (method == "smart"){
+            X_treatment <- paste0("X.", 2:6)
+            X_outcome <- paste0("X.", 2:3)
+          } else if (method == "minimal.set"){
+            X_treatment <- paste0("X.", 2:3)
+            X_outcome <- paste0("X.", 2:3)
+          } else {
+            stop("error in subset.")
+          }
       }
      
-      
-      # choose subset
-      for (method in different_subset_tested){
-        if (method == "outcome.and.instruments"){
-          X_treatment <- paste0("X.", 1:10)
-          X_outcome <- paste0("X.", 1:10)
-        } else if (method == "outcome.wo.instruments"){
-          X_treatment <- paste0("X.", 4:10)
-          X_outcome <- paste0("X.", 4:10)
-        } else if (method == "smart"){
-          X_treatment <- paste0("X.", 4:7)
-          X_outcome <- paste0("X.", 4:10)
-        } else if (method == "minimal.set"){
-          
-        } else {
-          stop("error in subset.")
-        }
         
         custom_aipw_linear <- aipw_linear(X_treatment, X_outcome, dataframe = a_simulation, n.folds = 2)
         custom_aipw_forest <- aipw_forest(X_treatment, X_outcome, dataframe = a_simulation, n.folds = 2, min.node.size.if.forest = 1)
