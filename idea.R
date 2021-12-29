771e39770fae2915d9ecc5823d98a1b15c864085
bcolnet <benedicte.colnet@gmail.com>
2021-12-30 00:17:58 +0100

    investigate

--- idea.R
+++ idea.R
@@ -19,8 +19,8 @@ results <- data.frame("sample.size" = c(),
                       "term.B" = c(),
                       "term.C" = c(),
                       "AIPW" = c(),
-                      "subset" = c(),
-                      "removing.extremes" = c())
+                      "g-formula" = c(),
+                      "subset" = c())
 
 different_subset_tested <- c("extended",
                              "smart",
@@ -33,60 +33,48 @@ for (sample.size in c(300, 1000, 2000, 5000, 10000)){
     # generate a simulation
     simulation <- generate_simulation_wager_nie(n = sample.size, setup = "A")
     
-    # choose subset
-    for (method in different_subset_tested){
-      if (method == "extended"){
-        X_treatment <- paste0("X.", 2:6)
-        X_outcome <- paste0("X.", 2:6)
-      } else if (method == "smart"){
-        X_treatment <- paste0("X.", 2:3)
-        X_outcome <- paste0("X.", 2:6)
-      } else if (method == "minimal"){
-        X_treatment <- paste0("X.", 2:3)
-        X_outcome <- paste0("X.", 2:3)
-      } else {
-        stop("error in subset.")
-      }
-      
-      # fit models
-      outcome.model.treated <-  ranger(Y ~ .,  
+    X.min <- paste0("X.", 2:3)
+    X.extend <- paste0("X.", 2:6)
+    
+    # fit models
+    outcome.model.treated <-  ranger(Y ~ .,  
                                        num.trees = 500, 
                                        max.depth = NULL,
                                        min.node.size = 1, 
-                                       data = simulation[simulation$A == 1, c("Y", X_outcome)])
-      outcome.model.control <-  ranger(Y ~ .,  
+                                       data = simulation[simulation$A == 1, c("Y", X.extend)])
+    outcome.model.control <-  ranger(Y ~ .,  
                                        num.trees = 500, 
                                        max.depth = NULL,
                                        min.node.size = 1, 
-                                       data = simulation[simulation$A == 0, c("Y", X_outcome)])
+                                       data = simulation[simulation$A == 0, c("Y", X.extend)])
       
-      propensity.model <- probability_forest(simulation[, X_treatment], 
+    propensity.model <- probability_forest(simulation[, X_treatment], 
                                              as.factor(simulation[, "A"]), 
                                              num.trees = 500, 
                                              min.node.size=1)
       
       
-      # prediction and estimation
-      simulation.to.estimate <- generate_simulation_wager_nie(n = 10000, setup = "A", all_covariates_output = TRUE)
-      mu.hat.1 <- predict(outcome.model.treated, simulation.to.estimate[, X_outcome])$predictions
-      bias.mu.1 <- mean(mu.hat.1-simulation.to.estimate$mu_1)
-      mu.hat.0 <- predict(outcome.model.control, simulation.to.estimate[, X_outcome])$predictions
-      bias.mu.0 <- mean(mu.hat.0-simulation.to.estimate$mu_0)
-      e.hat <- predict(propensity.model, 
+    # prediction and estimation
+    simulation.to.estimate <- generate_simulation_wager_nie(n = 10000, setup = "A", all_covariates_output = TRUE)
+    mu.hat.1 <- predict(outcome.model.treated, simulation.to.estimate[, X_outcome])$predictions
+    bias.mu.1 <- mean(mu.hat.1-simulation.to.estimate$mu_1)
+    mu.hat.0 <- predict(outcome.model.control, simulation.to.estimate[, X_outcome])$predictions
+    bias.mu.0 <- mean(mu.hat.0-simulation.to.estimate$mu_0)
+    e.hat <- predict(propensity.model, 
                        newdata = simulation.to.estimate[,X_treatment])$predictions[,2]
-      bias.e <- mean(1/e.hat-1/simulation.to.estimate$e)
+    bias.e <- mean(1/e.hat-1/simulation.to.estimate$e)
       
-      term.A <- mean( (simulation.to.estimate$mu_1 - mu.hat.1) * (1 - (simulation.to.estimate$A /simulation.to.estimate$e))  ) 
-      term.B <- mean( (simulation.to.estimate$Y_1 - simulation.to.estimate$mu_1) * ((1/e.hat) - (1/simulation.to.estimate$e))  )
-      term.C <- mean( (1/e.hat-1/simulation.to.estimate$e) * (mu.hat.1-simulation.to.estimate$mu_1) )
+    term.A <- mean( (simulation.to.estimate$mu_1 - mu.hat.1) * (1 - (simulation.to.estimate$A /simulation.to.estimate$e))  ) 
+    term.B <- mean( (simulation.to.estimate$Y_1 - simulation.to.estimate$mu_1) * ((1/e.hat) - (1/simulation.to.estimate$e))  )
+    term.C <- mean( (1/e.hat-1/simulation.to.estimate$e) * (mu.hat.1-simulation.to.estimate$mu_1) )
       
-      W <- simulation.to.estimate$A
-      Y <- simulation.to.estimate$Y
-      aipw.on.second.fold <- mean(mu.hat.1 - mu.hat.0
+    W <- simulation.to.estimate$A
+    Y <- simulation.to.estimate$Y
+    aipw.on.second.fold <- mean(mu.hat.1 - mu.hat.0
                                   + W / e.hat * (Y -  mu.hat.1)
                                   - (1 - W) / (1 - e.hat) * (Y -  mu.hat.0))
       
-      new_row <- data.frame("sample.size" = sample.size,
+    new_row <- data.frame("sample.size" = sample.size,
                             "bias.mu.1" = bias.mu.1,
                             "bias.mu.0" = bias.mu.0,
                             "bias.e" = bias.e,
@@ -94,12 +82,11 @@ for (sample.size in c(300, 1000, 2000, 5000, 10000)){
                             "term.B" = term.B,
                             "term.C" = term.C,
                             "AIPW" = aipw.on.second.fold,
-                            "subset" = method,
-                            "removing.extremes" = FALSE)
+                            "g-formula" = mean(mu.hat.1 - mu.hat.0),
+                            "subset" = method)
       
-      results <- rbind(results, new_row)
+    results <- rbind(results, new_row)
       
-    }
   }
 }
 
