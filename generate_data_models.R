3c7f1057cbc72b11e46f464804128090af17a5db
bcolnet <benedicte.colnet@gmail.com>
2021-12-30 16:52:12 +0100

    more variance

--- generate_data_models.R
+++ generate_data_models.R
@@ -42,7 +42,7 @@ generate_simulation_wager_nie <- function(n = 1000, p = 12, setup = "D", all_cov
     X = matrix(runif(n*p, min=0, max=1), n, p)
     b = sin(pi * X[,1] * X[,2]) + 2 * (X[,3] - 0.5)^2 
       + ifelse(X[,4] > 0, 3, -3) 
-        + 0.5 * ifelse(X[,5] > 0, 5, 0)
+        + ifelse(X[,5] > 0 & X[,6] < 0, 10, 20)
           + pmax(X[,6], 1)
     eta = 0.1
     e = pmax(eta, pmin(sin(pi * X[,1] * X[,2]), 1-eta))
