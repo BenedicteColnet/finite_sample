95499ed2ecc758ec009a4887944c91b4ba0de11a
bcolnet <benedicte.colnet@gmail.com>
2022-04-04 11:34:20 -0700

    complete simulations with 2 steps

---
+++ 2s.R
@@ -0,0 +1,70 @@
+# Reproducibility
+set.seed(123)
+
+# Libraries
+library(dplyr) # case_when and others
+library(ggplot2)
+library(tidyr) # pivot
+library(grf)
+library(glmnet)
+library(ranger) # efficient forest
+library(splines) # function bs() for splines
+library(mvtnorm) # rmvnorm
+library(tmle)
+library(SuperLearner)
+library(AIPW)
+
+source("estimators.R")
+source("generate_data_models.R")
+
+results.linear <- data.frame("sample.size" = c(),
+                             "estimate" = c(),
+                             "estimator" = c(),
+                             "subset" = c(),
+                             "nuisance" = c(),
+                             "term.A" = c(), 
+                             "term.B" = c(), 
+                             "term.C" = c(),
+                             "term.D" = c(), 
+                             "term.E" = c(), 
+                             "term.F" = c(),
+                             "setup" = c())
+
+different_subset_tested <- c("extended",
+                             "smart",
+                             "minimal")
+
+for (sample.size in c(100, 300, 1000, 3000, 10000)){
+  print(paste0("Starting sample size ", sample.size))
+  for (i in 1:50){
+    
+    for (wager in c("A", "C", "D")){
+      # generate a simulation
+      a_simulation <- generate_simulation_wager_nie(n = sample.size, setup = wager, all_covariates_output = TRUE)
+      
+      
+      estimate.two.steps <- aipw_two_steps(X_treatment, 
+                                           X_outcome,
+                                           dataframe = a_simulation,
+                                           min.node.size.if.forest = 1)
+      
+      new.row <- data.frame("sample.size" = sample.size,
+                            "estimate" = estimate.two.steps,
+                            "estimator" = "aipw - two steps",
+                            "subset" = "smart",
+                            "nuisance" = "forest",
+                            "term.A" = NA, 
+                            "term.B" = NA, 
+                            "term.C" = NA,
+                            "term.D" = NA, 
+                            "term.E" = NA, 
+                            "term.F" = NA,
+                            "setup" = wager)
+      
+      results.linear <- rbind(results.linear, new.row)
+      
+    }
+  }
+}
+
+write.csv(x=results.linear, file="./data/2s.csv")
