f364dbafd25c3998c4127a9dbcc11a36b2ae7c44
bcolnet <benedicte.colnet@gmail.com>
2022-04-25 21:13:02 -0700

    binned IPW

--- 2s.R
+++ 2s.R
@@ -51,25 +51,34 @@ for (sample.size in c(100, 300, 1000, 3000, 10000)){
       a_simulation <- generate_simulation_wager_nie(n = sample.size, setup = wager, all_covariates_output = TRUE)
       
       
-      estimate.two.steps <- binned_ipw(X_treatment, 
-                                       dataframe = a_simulation,
-                                       nb.bin = 10)
+      ipw.10 <- binned_ipw(X_treatment,
+                           dataframe = a_simulation,
+                           nb.bin = 10)
+      ipw.20 <- binned_ipw(X_treatment,
+                           dataframe = a_simulation,
+                           nb.bin = 20)
+      ipw.30 <- binned_ipw(X_treatment,
+                           dataframe = a_simulation,
+                           nb.bin = 30)
+      ipw.40 <- binned_ipw(X_treatment,
+                           dataframe = a_simulation,
+                           nb.bin = 40)
       
       estimate.cf <- causal_forest_wrapper(X_treatment, 
                                            dataframe = a_simulation)
       
-      new.row <- data.frame("sample.size" = sample.size,
-                            "estimate" = estimate.two.steps,
-                            "estimator" = "ipw - bin",
-                            "subset" = "minimal",
-                            "nuisance" = "forest",
-                            "term.A" = NA, 
-                            "term.B" = NA, 
-                            "term.C" = NA,
-                            "term.D" = NA, 
-                            "term.E" = NA, 
-                            "term.F" = NA,
-                            "setup" = wager)
+      new.row <- data.frame("sample.size" = rep(sample.size, 4),
+                            "estimate" = c(ipw.10, ipw.20, ipw.30, ipw.40),
+                            "estimator" = paste0("ipw.bin.", c("10", "20", "30", "40")),
+                            "subset" = rep("minimal", 4),
+                            "nuisance" = rep("forest", 4),
+                            "term.A" = rep(NA, 4), 
+                            "term.B" = rep(NA, 4),  
+                            "term.C" = rep(NA, 4), 
+                            "term.D" = rep(NA, 4),  
+                            "term.E" = rep(NA, 4),  
+                            "term.F" = rep(NA, 4), 
+                            "setup" = rep(wager, 4))
       
       new.row.bis <- data.frame("sample.size" = sample.size,
                             "estimate" = estimate.cf,
